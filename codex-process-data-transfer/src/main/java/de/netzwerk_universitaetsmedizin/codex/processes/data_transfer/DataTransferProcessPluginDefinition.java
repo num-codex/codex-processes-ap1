debe3d2fc2a4a86f5528fde32409991a7279406e
Hauke Hund <hauke.hund@hs-heilbronn.de>
2021-08-24 03:36:22 +0200

    switching version to 0.4.0

--- codex-process-data-transfer/src/main/java/de/netzwerk_universitaetsmedizin/codex/processes/data_transfer/DataTransferProcessPluginDefinition.java
+++ codex-process-data-transfer/src/main/java/de/netzwerk_universitaetsmedizin/codex/processes/data_transfer/DataTransferProcessPluginDefinition.java
@@ -21,7 +21,7 @@ import de.netzwerk_universitaetsmedizin.codex.processes.data_transfer.spring.con
 
 public class DataTransferProcessPluginDefinition implements ProcessPluginDefinition
 {
-	public static final String VERSION = "0.5.0";
+	public static final String VERSION = "0.4.0";
 
 	@Override
 	public String getName()
