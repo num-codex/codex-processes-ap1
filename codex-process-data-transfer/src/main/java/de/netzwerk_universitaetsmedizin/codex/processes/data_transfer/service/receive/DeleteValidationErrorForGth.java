1191ff668561728df95e34c3f5466e5127310fbf
Hauke Hund <hauke.hund@hs-heilbronn.de>
2022-06-20 14:13:25 +0200

    deactivated delete steps due to validation errors when updating tasks
    For this to work, the DSF needs a mechanism to ignore unknown references
    when updating task resources after deleting a referenced binary or we
    need to delete the binaries after all Task resource have been updated.

--- codex-process-data-transfer/src/main/java/de/netzwerk_universitaetsmedizin/codex/processes/data_transfer/service/receive/DeleteValidationErrorForGth.java
+++ codex-process-data-transfer/src/main/java/de/netzwerk_universitaetsmedizin/codex/processes/data_transfer/service/receive/DeleteValidationErrorForGth.java
@@ -1,21 +1,15 @@
 package de.netzwerk_universitaetsmedizin.codex.processes.data_transfer.service.receive;
 
-import static de.netzwerk_universitaetsmedizin.codex.processes.data_transfer.ConstantsDataTransfer.BPMN_EXECUTION_VARIABLE_BINARY_URL;
-
 import org.camunda.bpm.engine.delegate.BpmnError;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.highmed.dsf.bpe.delegate.AbstractServiceDelegate;
 import org.highmed.dsf.fhir.authorization.read.ReadAccessHelper;
 import org.highmed.dsf.fhir.client.FhirWebserviceClientProvider;
 import org.highmed.dsf.fhir.task.TaskHelper;
-import org.hl7.fhir.dstu3.model.IdType;
-import org.hl7.fhir.r4.model.Binary;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class DeleteValidationErrorForGth extends AbstractServiceDelegate
 {
-	private static final Logger logger = LoggerFactory.getLogger(DeleteValidationErrorForGth.class);
+	// private static final Logger logger = LoggerFactory.getLogger(DeleteValidationErrorForGth.class);
 
 	public DeleteValidationErrorForGth(FhirWebserviceClientProvider clientProvider, TaskHelper taskHelper,
 			ReadAccessHelper readAccessHelper)
@@ -26,12 +20,14 @@ public class DeleteValidationErrorForGth extends AbstractServiceDelegate
 	@Override
 	protected void doExecute(DelegateExecution execution) throws BpmnError, Exception
 	{
-		String binaryUrl = (String) execution.getVariable(BPMN_EXECUTION_VARIABLE_BINARY_URL);
-		IdType binaryId = new IdType(binaryUrl);
+		// String binaryUrl = (String) execution.getVariable(BPMN_EXECUTION_VARIABLE_BINARY_URL);
+		// IdType binaryId = new IdType(binaryUrl);
+		//
+		// logger.info("Deleting validation error binary {} permanently", binaryId.getValue());
+		// getFhirWebserviceClientProvider().getLocalWebserviceClient().delete(Binary.class, binaryId.getIdPart());
+		// getFhirWebserviceClientProvider().getLocalWebserviceClient().deletePermanently(Binary.class,
+		// binaryId.getIdPart());
 
-		logger.info("Deleting validation error binary {} permanently", binaryId.getValue());
-		getFhirWebserviceClientProvider().getLocalWebserviceClient().delete(Binary.class, binaryId.getIdPart());
-		getFhirWebserviceClientProvider().getLocalWebserviceClient().deletePermanently(Binary.class,
-				binaryId.getIdPart());
+		// TODO implement delete, cave possible task update with reference to delete binary
 	}
 }
