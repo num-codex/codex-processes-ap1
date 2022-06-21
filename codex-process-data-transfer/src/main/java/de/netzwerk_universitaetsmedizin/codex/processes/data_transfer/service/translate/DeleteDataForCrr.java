d716ada93d0a179d9dac976e2d97611806716532
Hauke Hund <hauke.hund@hs-heilbronn.de>
2022-06-21 23:28:48 +0200

    removed not needed log entries

--- codex-process-data-transfer/src/main/java/de/netzwerk_universitaetsmedizin/codex/processes/data_transfer/service/translate/DeleteDataForCrr.java
+++ codex-process-data-transfer/src/main/java/de/netzwerk_universitaetsmedizin/codex/processes/data_transfer/service/translate/DeleteDataForCrr.java
@@ -1,7 +1,6 @@
 package de.netzwerk_universitaetsmedizin.codex.processes.data_transfer.service.translate;
 
 import static de.netzwerk_universitaetsmedizin.codex.processes.data_transfer.ConstantsDataTransfer.BPMN_EXECUTION_VARIABLE_BINARY_URL;
-import static de.netzwerk_universitaetsmedizin.codex.processes.data_transfer.ConstantsDataTransfer.BPMN_EXECUTION_VARIABLE_CONTINUE_STATUS;
 
 import org.camunda.bpm.engine.delegate.BpmnError;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
@@ -13,8 +12,6 @@ import org.hl7.fhir.r4.model.Binary;
 import org.hl7.fhir.r4.model.IdType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import de.netzwerk_universitaetsmedizin.codex.processes.data_transfer.service.ContinueStatus;
 
 public class DeleteDataForCrr extends AbstractServiceDelegate
 {
@@ -29,9 +26,6 @@ public class DeleteDataForCrr extends AbstractServiceDelegate
 	@Override
 	protected void doExecute(DelegateExecution execution) throws BpmnError, Exception
 	{
-		ContinueStatus continueStatus = (ContinueStatus) execution.getVariable(BPMN_EXECUTION_VARIABLE_CONTINUE_STATUS);
-		logger.info("Continue status: {}", continueStatus);
-
 		String binaryUrl = (String) execution.getVariable(BPMN_EXECUTION_VARIABLE_BINARY_URL);
 		IdType binaryId = new IdType(binaryUrl);
 
