f27bb7b5f66d674287131a702adb1b427321d6a3
Hauke Hund <hauke.hund@hs-heilbronn.de>
2022-07-03 17:04:54 +0200

    workaround for adding missing codes to expanded ValueSet
    Fixes error with ValueSets
    https://www.netzwerk-universitaetsmedizin.de/fhir/ValueSet/rheumatological-immunological-diseases
    and https://www.netzwerk-universitaetsmedizin.de/fhir/ValueSet/vaccines-snomed

---
+++ codex-process-data-transfer/src/main/java/de/netzwerk_universitaetsmedizin/codex/processes/data_transfer/validation/value_set/MissingEntriesIncluder.java
@@ -0,0 +1,69 @@
+package de.netzwerk_universitaetsmedizin.codex.processes.data_transfer.validation.value_set;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.hl7.fhir.r4.model.ValueSet;
+import org.hl7.fhir.r4.model.ValueSet.ConceptSetComponent;
+import org.hl7.fhir.r4.model.ValueSet.ValueSetExpansionContainsComponent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MissingEntriesIncluder implements ValueSetModifier
+{
+	private static final Logger logger = LoggerFactory.getLogger(MissingEntriesIncluder.class);
+
+	@Override
+	public ValueSet modifyPostExpansion(ValueSet vsWithComposition, ValueSet vsWithExpansion)
+	{
+		if (vsWithExpansion == null)
+			return null;
+		if (!vsWithExpansion.hasExpansion())
+			return vsWithExpansion;
+
+		if (vsWithComposition.hasCompose()
+				&& vsWithComposition.getCompose().getInclude().stream().anyMatch(ConceptSetComponent::hasConcept))
+		{
+			Set<String> expandedEntries = vsWithExpansion.getExpansion().getContains().stream()
+					.map(c -> toEntry(c.getSystem(), c.getVersion(), c.getCode())).distinct()
+					.collect(Collectors.toSet());
+
+			vsWithComposition.getCompose().getInclude().stream().filter(ConceptSetComponent::hasConcept)
+					.forEach(include ->
+					{
+						String system = include.getSystem();
+						String version = include.getVersion();
+
+						include.getConcept().forEach(concept ->
+						{
+							if (!expandedEntries.contains(toEntry(system, version, concept.getCode())))
+							{
+								if (version != null)
+									logger.warn(
+											"Adding missing concept to ValueSet {}|{}: system: '{}', version: '{}', code: '{}', display: '{}'",
+											vsWithExpansion.getUrl(), vsWithExpansion.getVersion(), system, version,
+											concept.getCode(), concept.getDisplay());
+								else
+									logger.warn(
+											"Adding missing concept to ValueSet {}|{}: system: '{}', code: '{}', display: '{}'",
+											vsWithExpansion.getUrl(), vsWithExpansion.getVersion(), system,
+											concept.getCode(), concept.getDisplay());
+
+								vsWithExpansion.getExpansion()
+										.addContains(new ValueSetExpansionContainsComponent().setSystem(system)
+												.setVersion(version).setCode(concept.getCode())
+												.setDisplay(concept.getDisplay()));
+							}
+						});
+					});
+		}
+
+		return vsWithExpansion;
+	}
+
+	private String toEntry(String system, String version, String code)
+	{
+		return Stream.of(system, version, code).filter(e -> e != null).collect(Collectors.joining());
+	}
+}
