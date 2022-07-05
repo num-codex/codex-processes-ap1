cd15c7c06065926ef4387861b28fc1829da1f06b
Hauke Hund <hauke.hund@hs-heilbronn.de>
2022-07-05 21:47:46 +0200

    adds dry-run functionality to data-send process
    The data-send process can now be instructed to perform a dry-run only
    using the optional input-parameter "dry-run" with a value of "true". A
    dry-run will abort the data-send process after validation end enryption
    of the transfer bundle but before the bundle is stored in the local DSF
    FHIR server and a Task to start the data-translate process is send to
    the GECCO Transfer Hub.

---
+++ codex-process-data-transfer/src/main/java/de/netzwerk_universitaetsmedizin/codex/processes/data_transfer/service/send/CheckDryRun.java
@@ -0,0 +1,36 @@
+package de.netzwerk_universitaetsmedizin.codex.processes.data_transfer.service.send;
+
+import static de.netzwerk_universitaetsmedizin.codex.processes.data_transfer.ConstantsDataTransfer.BPMN_EXECUTION_VARIABLE_DRY_RUN;
+import static de.netzwerk_universitaetsmedizin.codex.processes.data_transfer.ConstantsDataTransfer.CODESYSTEM_NUM_CODEX_DATA_TRANSFER;
+import static de.netzwerk_universitaetsmedizin.codex.processes.data_transfer.ConstantsDataTransfer.CODESYSTEM_NUM_CODEX_DATA_TRANSFER_VALUE_DRY_RUN;
+
+import java.util.Optional;
+
+import org.camunda.bpm.engine.delegate.BpmnError;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.variable.Variables;
+import org.highmed.dsf.bpe.delegate.AbstractServiceDelegate;
+import org.highmed.dsf.fhir.authorization.read.ReadAccessHelper;
+import org.highmed.dsf.fhir.client.FhirWebserviceClientProvider;
+import org.highmed.dsf.fhir.task.TaskHelper;
+import org.hl7.fhir.r4.model.Task;
+
+public class CheckDryRun extends AbstractServiceDelegate
+{
+	public CheckDryRun(FhirWebserviceClientProvider clientProvider, TaskHelper taskHelper,
+			ReadAccessHelper readAccessHelper)
+	{
+		super(clientProvider, taskHelper, readAccessHelper);
+	}
+
+	@Override
+	protected void doExecute(DelegateExecution execution) throws BpmnError, Exception
+	{
+		Task task = getLeadingTaskFromExecutionVariables();
+
+		Optional<Boolean> dryRun = getTaskHelper().getFirstInputParameterBooleanValue(task,
+				CODESYSTEM_NUM_CODEX_DATA_TRANSFER, CODESYSTEM_NUM_CODEX_DATA_TRANSFER_VALUE_DRY_RUN);
+
+		execution.setVariable(BPMN_EXECUTION_VARIABLE_DRY_RUN, Variables.booleanValue(dryRun.orElse(Boolean.FALSE)));
+	}
+}
