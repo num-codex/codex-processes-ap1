39d54a4257f92de0c48d6e118c4ab6a652bd4700
Marcus Fabarius <marcus.fabarius@sva.de>
2024-08-07 10:46:02 +0200

    update error log massage, add target store key

--- codex-process-data-transfer/src/main/java/de/netzwerk_universitaetsmedizin/codex/processes/data_transfer/service/receive/InsertDataIntoCodex.java
+++ codex-process-data-transfer/src/main/java/de/netzwerk_universitaetsmedizin/codex/processes/data_transfer/service/receive/InsertDataIntoCodex.java
@@ -12,7 +12,6 @@ import org.hl7.fhir.r4.model.Task;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import de.netzwerk_universitaetsmedizin.codex.processes.data_transfer.ConstantsDataTransfer;
 import de.netzwerk_universitaetsmedizin.codex.processes.data_transfer.client.DataStoreClientFactory;
 import de.netzwerk_universitaetsmedizin.codex.processes.data_transfer.client.fhir.ValidationException;
 import de.netzwerk_universitaetsmedizin.codex.processes.data_transfer.logging.DataLogger;
@@ -78,9 +77,9 @@ public class InsertDataIntoCodex extends AbstractServiceDelegate
 		}
 		catch (Exception e)
 		{
-			logger.warn("Unable to insert data into CRR: {} - {}", e.getClass().getName(), e.getMessage());
+			logger.warn("Unable to insert data into '{}': {} - {}", studyId, e.getClass().getName(), e.getMessage());
 			throw new BpmnError(CODESYSTEM_NUM_CODEX_DATA_TRANSFER_ERROR_VALUE_INSERT_INTO_CRR_FHIR_REPOSITORY_FAILED,
-					"Unable to insert data into CRR");
+					"Unable to insert data into '" + studyId + "'");
 		}
 	}
 
